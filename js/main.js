27d7e3f93e3d27685049830abb9b86f9b29b4ae6
Sebastian Björkqvist <sebastian.bjorkqvist@helsinki.fi>
2017-02-22 17:25:11 +0200

    First working verison with hard-coded start and end dates

---
+++ js/main.js
@@ -0,0 +1,67 @@
+var params = {
+    apiBaseURL: 'http://avoindata.prh.fi:80/tr/v1/publicnotices',
+    tableId: 'resultTable'
+};
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages': ['table']});
+
+function drawTable (tableId, data) {
+    var table = new google.visualization.Table(document.getElementById(tableId));    
+    table.draw(data, {width: '100%', height: '100%', sortColumn: 2, sortAscending: true});    
+};
+
+function createTableDataObject  () {
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Business id');
+    data.addColumn('string', 'Company name');
+    data.addColumn('string', 'Bankruptcy date');
+
+    return data;
+};
+
+function clearTableRows (tableId) {
+    $(tableId).find('tbody').remove();
+    $(tableId).append('<tbody></tbody>');
+};
+
+function addEntryRowToTable (entry, tableData) {
+    tableData.addRow([entry.businessId, entry.name, entry.registrationDate]);
+};
+
+function getBankruptcies (amount) {
+    var tableId = params.tableId;
+    $.ajax({
+        type: 'GET',
+        url: getFullUrl(amount, "2016-01-01", "2016-12-31"),
+        dataType: 'json',
+        success: function (response) {
+            var tableData = createTableDataObject();
+            $.each(response.results, function (index, entry) {
+                addEntryRowToTable(entry, tableData);
+            });
+            drawTable(tableId, tableData);
+        },
+        error: function (error) {
+            swal('Error when loading bankruptcies', 'Error message: ' + JSON.stringify(error), 'error');
+            console.log(error);
+        }
+
+    });
+};
+
+function getFullUrl(numberOfResults, startDate, endDate) {
+    var fullUrl = params.apiBaseURL + "?totalResults=true";
+    fullUrl += "&entryCode=KONALK";
+    if (numberOfResults !== undefined && numberOfResults !== null) {
+        fullUrl += "&maxResults=" + numberOfResults;
+    }
+    if (startDate !== undefined && startDate !== null) {
+        fullUrl += "&noticeRegistrationFrom=" + startDate;
+    }
+    if (endDate !== undefined && endDate !== null) {
+        fullUrl += "&noticeRegistrationTo=" + endDate;
+    }
+    return fullUrl;
+}
